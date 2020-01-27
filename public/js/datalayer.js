e85e1c87450f2277291f30c70be136956809fdd5
Robert Mela <robert.steven.mela@gmail.com>
2020-01-26 19:01:09 -0500

    Comments, cleanup

--- public/js/datalayer.js
+++ public/js/datalayer.js
@@ -1,6 +1,15 @@
+/*
+ * Data Access Layer for MBTA Departure Board project
+ */
 const DataLayer = function() {
-  
+
   const BASE = 'https://api-v3.mbta.com'
+
+  /*
+   * API_KEY required for live prediction updates.
+   * You can hard-code it here, or you can pass it as an "apiKey" parameter in a query string.
+   *
+   */
 
   let API_KEY = "" // Put your API key here, or pass apiKey in query string
 
@@ -22,6 +31,13 @@ const DataLayer = function() {
   let predictionsUrl = `${BASE}/predictions/?filter[stop]=South Station,North Station&sort=time`
   let streamUrl      = `${BASE}/predictions/?filter[stop]=South Station,North Station&filter[direction_id]=0&api_key=${API_KEY}`
 
+  /**
+  * @flatten
+  *
+  * Extract information we want from prediction object, store in in a simpler structure that's easier
+  * to work with
+  */
+
   function flatten( prediction ) {
 
     let train = prediction.relationships.vehicle.data ?
@@ -30,7 +46,7 @@ const DataLayer = function() {
     // Some trains show as block_1234_something_something instead of 1234
     // Not sure what that means or whether to extract the train number from it
 
-    if( train.startsWith('block_') ) { 
+    if( train.startsWith('block_') ) {
       train = train.split('_')[1]
     }
 
@@ -45,9 +61,14 @@ const DataLayer = function() {
       trip: prediction.relationships.trip.data.id
     }
   } // flatten
-  
-  /*
-   * Fetch initial batch of predictions to initialize board 
+
+  /**
+   * @fetchPredictions
+   *
+   * Fetch batch of predictions to initialize board
+   *
+   * Invoked when application first loads.
+   *
    */
 
   async function fetchPredictions() {
@@ -63,42 +84,69 @@ const DataLayer = function() {
       //
       // For now just filter them out.
       //
-      .filter( elt => elt.departure ) 
+      .filter( elt => elt.departure )
   }
 
-  /*
-   * Start receiving updates via stream
+  /**
+   * @startEventStream
+   *
+   * Start receiving preduction updates via event stream
+   *
    */
 
   startEventStream = () => {
 
     if(!API_KEY) return
- 
+
     this.eventStream = new EventSource( streamUrl )
-    this.eventStream.onmessage = function(msg) { console.log('message', msg ) }
+
     this.eventStream.onclose = function(msg) { console.log('close', msg ) }
     this.eventStream.onopen = function(msg) { console.log('open', msg ) }
     this.eventStream.addEventListener('error', function(err) {
        console.error( err )
     })
+
     this.eventStream.addEventListener('update', this.onUpdate )
 
   }
 
-  /*
-   * Remove predictions where departure time is in the past
+  /**
+   *
+   * @prune
+   *
+   * Remove predictions for departed trains (departure time is in the past)
+   *
    */
 
   function prune() {
-    let pruned = DATA.predictions.filter( p => p.departure > moment().format() )
-    if( pruned.length == DATA.predictions.length ) return
-console.log( `pruned ${DATA.predictions.length - pruned.length} items` )
+
+    let cutoff = moment()
+    cutoff.add( -2, 'minutes' )
+
+    let pruned = DATA.predictions.filter( p => { // Filter for non-departed trains
+
+       // Assumes departures in list are never null.  Not sure that's a safe assumption.
+ 
+       let t = moment(p.departure);
+       return t  > cutoff
+
+    })
+
+    if( pruned.length == DATA.predictions.length ) { // No departed trains. Nothing to prune.
+      return
+    }
+
+    console.log( `pruned ${DATA.predictions.length - pruned.length} items` )
     DATA.predictions = pruned
   }
 
   /*
+   * @timeSortComparator 
+   *
    * Comparator for sorting predictions on departure time
+   *
    */
+
   function timeSortComparator(a, b ) {
      a = a.departure
      b = b.departure
@@ -108,27 +156,50 @@ console.log( `pruned ${DATA.predictions.length - pruned.length} items` )
             0
   }
 
+  /**
+  * @onUpdate
+  *
+  * Invoked when new predictions arrive on live stream.
+  *
+  * If matching prediction found in list, update it's values
+  *
+  * If no match found, then prediction is new.  Add it to the list.
+  *
+  */
+
   function onUpdate( msg ) {
-console.log( 'UPDATE', moment().format() )
-console.dir( msg.data )
+
+    console.log( 'UPDATE', moment().format() )
+    console.dir( msg.data )
+
     let data = flatten( JSON.parse( msg.data ) )
 
-    // predictions identified by trip (?) since track number is part of id, but couldn't
-    // train be sent to a different track?
+    let prediction = DATA.predictions.find( p => p.id == data.id )
 
-    let prediction = DATA.predictions.find( p => p.trip == data.trip )
     if( !prediction ) {
-console.log('Adding new prediction', data )
+
+       console.log('Adding new prediction', data )
        DATA.predictions.push( data )
        DATA.predictions = DATA.predictions.sort( timeSortComparator )
+
     } else {
-console.log('Updating existing prediction BEFORE', prediction )
+
+       console.log('Updating existing prediction BEFORE', prediction )
        if( data.departure ) prediction.departure = data.departure
        if( data.train ) prediction.train = data.train
        if( data.status )  prediction.status = data.status
-console.log('Updating existing prediction AFTER', prediction )
+       console.log('Updating existing prediction AFTER', prediction )
+
     }
   }
+
+  /**
+  *
+  * @init
+  *
+  * Application will call this function when it's ready to start using data.
+  *
+  */
 
   async function init() {
     await fetchPredictions()
